cf58fbeb0682c6cc34ff13cd6a2c48682d9d5df1
Ucine Hamouni <62403196+UCINE@users.noreply.github.com>
2024-01-31 17:39:01 +0000

    ex00

---
+++ python_module00/ex00/Hello.py
@@ -0,0 +1,9 @@
+ft_list = ["Hello", "World!"]
+ft_tuple = ("Hello", "Morocco")
+ft_set = {"Hello", "Sa7ra_Bengurir"}
+ft_dict = {"Hello" : "1337"}
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
