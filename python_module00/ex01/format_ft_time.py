b43d801e68b60c16b499b0866623366659f78881
Ucine Hamouni <62403196+UCINE@users.noreply.github.com>
2024-01-31 18:24:56 +0000

    fixing the format

--- python_module00/ex01/format_ft_time.py
+++ python_module00/ex01/format_ft_time.py
@@ -3,10 +3,11 @@ from datetime import datetime
 
 today = datetime.now()
 
+thisformat = today.strftime("%b %d %Y")
 def format_time():
     seconds = time.time()
 
     print(f"Seconds since January 1, 1970: {seconds} or {seconds:.2e} in scientific notation")
 
 format_time()
-print(today)
+print(thisformat)
