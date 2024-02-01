90839a826f5bab553a2f8c048056acd19745d515
Ucine Hamouni <62403196+UCINE@users.noreply.github.com>
2024-02-01 19:17:57 +0000

    remove dir

--- Module00/ex00/Hello.py
+++
@@ -1,9 +0,0 @@
-ft_list = ["Hello", "World!"]
-ft_tuple = ("Hello", "Morocco")
-ft_set = {"Hello", "Sa7ra_Bengurir"}
-ft_dict = {"Hello" : "1337"}
-
-print(ft_list)
-print(ft_tuple)
-print(ft_set)
-print(ft_dict)
