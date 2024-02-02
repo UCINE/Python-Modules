30e146e0fb4f82b377aa36e1b82243de61bef62b
Ucine Hamouni <62403196+UCINE@users.noreply.github.com>
2024-02-02 20:11:23 +0000

    done

--- python_module00/ex05/building.py
+++ python_module00/ex05/building.py
@@ -4,7 +4,29 @@ import string
 
 #I'm too lazy when it come to Check for command line arguments and handle shitty cases :/
 
-def letscountig(str)
+def letscountig(str):
 
+    text = sum(1 for char in str)
     spaces = str.count(' ')
-    numbers = sum(1 for char in str if char.isdigit()) 
+    numbers = sum(1 for char in str if char.isdigit())
+    upper = sum(1 for char in str if char.isupper())
+    lower = sum(1 for char in str if char.islower())
+    punctuation = sum(1 for char in str if char in string.punctuation)
+
+    print(f"The text contain {text} characters")
+    print(f"uppercase  : {upper}")
+    print(f"Lowercase  : {lower}")
+    print(f"Numbers    : {numbers}")
+    print(f"punctuation: {punctuation}")
+    print(f"Spaces     : {spaces}")
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        raise AssertionError("Only one argument :)")
+    if len(sys.argv) == 2:
+        result = sys.argv[1]
+    else:
+        result = print("Enetr a string ")
+    
+    letscountig(result)
+
