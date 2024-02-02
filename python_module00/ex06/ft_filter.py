cf4d6b282be62366a6ba024d298564fcc0242022
Ucine Hamouni <62403196+UCINE@users.noreply.github.com>
2024-02-02 21:53:46 +0000

    fixing ft_filter to work only if it get executed directly

--- python_module00/ex06/ft_filter.py
+++ python_module00/ex06/ft_filter.py
@@ -3,7 +3,8 @@
 def ft_filter(f_de_x, les_x):
     return [x for x in les_x if f_de_x(x)]
 
-xx = [1, -2, -3, 4]
-xr = ft_filter(lambda x: x < 0, xx)
+if __name__ == "__main__":
+    xx = [1, -2, -3, 4]
+    xr = ft_filter(lambda x: x < 0, xx)
 
-#print(xr)
+    print(xr)
